cd135ca64ccd79a8ca0a17f61056b99ab56f85fa
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-04-16 01:19:02 +0200

    Sup: analysis and sending code to Sonarqube Server

---
+++ sample_project/sonar-update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+MODULE=project
+TESTS=tests
+PYTHONPATH=`pwd`
+nosetests --with-xunit --xunit-file=$TESTS/nosetests.xml --with-coverage --cover-xml-file=coverage.xml --where $TESTS
+pylint $MODULE -r n --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" > pylint_report.txt
+bandit -r $MODULE --format json >bandit_report.json
+sonar-scanner -Dproject.settings=sonar-project.properties 
