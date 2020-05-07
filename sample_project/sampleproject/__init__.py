90c22108ca45b7511b787e026a32032ef7000604
Leonard Rothacker <leonard.rothacker@googlemail.com>
2020-05-07 15:35:42 +0200

    Externalized sample_project dir to dev-ops project

--- sample_project/sampleproject/__init__.py
+++
@@ -1,7 +0,0 @@
-
-import logging
-
-LOGGING_FORMAT = '%(asctime)-15s: [%(name)s] %(message)s'
-# LOGGING_FORMAT = '[%(name)s] %(message)s'
-logging.basicConfig(level=logging.INFO,
-                    format=LOGGING_FORMAT)
